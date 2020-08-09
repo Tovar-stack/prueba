81f4679181b555a843626996018d2da992ba0a30
Robert Tovar <57671052+Tovar-stack@users.noreply.github.com>
2020-08-09 18:52:15 -0500

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -1,7 +1,7 @@
 <!-- Barra de navegacion Materialize-->
 <nav>
     <div  class="nav-wrapper">
-      <a href="#" class="brand-logo"><img src="../images/Logo.png"></a>
+      <a href="#" class="brand-logo"><img src="images/Logo.png"></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a class="waves-effect" href="#">Flights</a></li>
